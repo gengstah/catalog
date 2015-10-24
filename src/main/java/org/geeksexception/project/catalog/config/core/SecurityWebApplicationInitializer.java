12064a880059a29715416d1c8f11c60c9bb502ff
gengstah <gerardpdelasarmas@gmail.com>
2015-10-24 11:23:37 +0800

    Added SecurityWebApplicationInitializer

---
+++ src/main/java/org/geeksexception/project/catalog/config/core/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package org.geeksexception.project.catalog.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends
+		AbstractSecurityWebApplicationInitializer {
+
+}
